6f915c3c079bef59f094220a46b9482f6e22101d
여름 <pjw7200@ajou.ac.kr>
2023-05-20 14:03:55 +0900

    second version of #9996

--- week1/9996.cpp
+++ week1/9996.cpp
@@ -4,35 +4,23 @@
 
 using namespace std;
 
-vector<string> split(string input, string deleimeter){
-    vector<string> ret;
-    long long pos = 0;
-    string token = "";
-    while((pos = input.find(deleimeter)) != string::npos){
-        token = input.substr(0, pos);
-        ret.push_back(token);
-        input.erase(0, pos + deleimeter.length());
-    }
-    ret.push_back(input);
-    return ret;
-}
-
 int n;
+string pre, suf;
 int main(void){
     cin >> n;
-    
-    string pat;
-    cin >> pat;
+    string s;
+    cin >> s;
 
-    vector<string> pattern = split(pat, "*");
+    int pos = s.find("*");
+    pre = s.substr(0, pos);
+    suf = s.substr(pos + 1);
+
     string temp;
-    int flen, slen, len;
+    int len;
     for(int i = 0; i < n; i++){
         cin >> temp;
         len = temp.size();
-        flen = pattern[0].size();
-        slen = pattern[1].size();
-        if(flen > len - slen || pattern[0] != temp.substr(0, flen) || pattern[1] != temp.substr(len - slen, slen))
+        if(pre.size() + suf.size() > len || pre != temp.substr(0, pre.size()) || suf != temp.substr(len - suf.size()))
             cout << "NE" << '\n';
         else
             cout << "DA" << '\n';
