4b4ba0317ba2bec466b657e02169715e399fb096
여름 <pjw7200@ajou.ac.kr>
2023-05-20 16:11:50 +0900

    first version of #1940

---
+++ week1/1940.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int n, m;
+int num[15000];
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+    cin >> m;
+    int a;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        num[i] = a;
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(num[i] + num[j] == m)
+                cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
