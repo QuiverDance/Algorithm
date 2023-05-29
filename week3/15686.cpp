999f3ee5d921bbb91cff63a4943550972a46190d
여름 <pjw7200@ajou.ac.kr>
2023-05-29 14:49:20 +0900

    frist version of #15686

--- week3/15686.cpp
+++ week3/15686.cpp
@@ -3,53 +3,46 @@
 
 using namespace std;
 
-int n, m;
-int ret = 1000000;
-int board[50][50], visitied[50][50];
-vector<int> v;
-vector<pair<int, int>> homeList, chickenList;
+int n, m, a[54][54], result = 10000000;
+vector<vector<int>>chickenList;
+vector<pair<int, int>> home, chicken;
 
-int getDistance(pair<int, int> from, pair<int, int> to){
-    return abs(to.first - from.first) + abs(to.second - from.second);
-}
-void go(int index){
-    if(index == chickenList.size())
+void combi(int start, vector<int> v){
+    if(v.size() == m){
+        chickenList.push_back(v);
         return;
-    if(v.size() > m)
-        return;
-
-    int sum = 0, distance;
-    for(auto h : homeList){
-        int min = 101;
-        for(int c : v){
-            distance = getDistance(h, chickenList[c]);
-            if(min > distance) min = distance;
-            if(distance == 1)
-                break;
-        }
-        sum += min;
     }
-    ret = min(ret, sum);
-
-    v.push_back(index + 1);
-    go(index + 1);
-    v.pop_back();
-    go(index + 1);
+    for(int i = start + 1; i < chicken.size(); i++){
+        v.push_back(i);
+        combi(i, v);
+        v.pop_back();
+    }
+    return;
 }
 
-int main(void){
+int main(){
     cin >> n >> m;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            cin >> board[i][j];
-            if(board[i][j] == 1) homeList.push_back({i, j});
-            else if(board[i][j] == 2) chickenList.push_back({i, j});
+            cin >> a[i][j];
+            if(a[i][j] == 1)home.push_back({i, j});
+            if(a[i][j] == 2)chicken.push_back({i, j});
         }
     }
-    v.push_back(0);
-    go(0);
-    v.pop_back();
-    go(0);
-    cout << ret;
+    vector<int> v;
+    combi(-1, v);
+    for(vector<int> cList : chickenList){
+        int ret = 0;
+        for(pair<int, int> h : home){
+            int _min = 101;
+            for(int ch : cList){
+                int _dist = abs(h.first - chicken[ch].first) + abs(h.second - chicken[ch].second);
+                _min = min(_min, _dist);
+            }
+            ret += _min;
+        }
+        result = min(result, ret);
+    }
+    cout << result << "\n";
     return 0;
 }
