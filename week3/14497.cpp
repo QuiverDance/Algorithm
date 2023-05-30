b172c1342456c3a8e9a04b070b3e6acbfe05b93e
여름 <pjw7200@ajou.ac.kr>
2023-05-31 00:38:57 +0900

    second version of #14497

--- week3/14497.cpp
+++ week3/14497.cpp
@@ -1,54 +1,48 @@
-#include<iostream>
-#include<string>
-#include<cstring>
+#include <stdio.h>
+#include<algorithm>
+#include<queue>
 
-using namespace std;
+using namespace std; 
 
-int n, m, cnt, flag;
-string s;
-int board[300][300], visited[300][300];
-pair<int, int> jpos, tpos;
+char a[301][301];
+int n, m, x1, y1, x2, y2; 
+typedef pair<int, int> pii;
+int visited[301][301];
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
+int ret; 
+queue<int> q;
 
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-
-void dfs(int y, int x){
-    if(y == tpos.first && x == tpos.second){
-        flag = 1;
-        return;
-    }
-    visited[y][x] = 1;
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny > n - 1 || nx > m - 1) continue;
-        if(visited[ny][nx]) continue;
-        if(board[ny][nx] == '1'){
-            visited[ny][nx] = 1;
-            board[ny][nx] = '0';
-        }
-        else dfs(ny, nx);
-    }
-}
-
-int main(void){
-    int x1, y1, x2, y2;
-    cin >> n >> m;
-    cin >> y1 >> x1 >> y2 >> x2;
-    jpos = {y1-1, x1-1};
-    tpos = {y2-1, x2-1};
+int main(){
+    scanf("%d %d", &n, &m);
+    scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
+    y1--, x1--, y2--, x2--; 
     for(int i = 0; i < n; i++){
-        cin >> s;
-        for(int j = 0; j < m; j++){
-            board[i][j] = s[j];
-        }
-    }
-    flag = 0; cnt = 0;
-    while(!flag){
+        scanf("%s", a[i]); 
+    }  
+    q.push(1000 * y1 + x1);
+    visited[y1][x1] = 1; 
+    int cnt = 0; 
+    while(a[y2][x2] != '0'){ 
         cnt++;
-        memset(visited, 0, sizeof(visited));
-        dfs(jpos.first, jpos.second);
+        queue<int> temp; 
+        while(q.size()){
+            int y = q.front() / 1000; 
+            int x = q.front() % 1000;  
+            q.pop();  
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i]; 
+                int nx = x + dx[i];
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue; 
+                visited[ny][nx] = cnt;  
+                if(a[ny][nx] != '0'){
+                    a[ny][nx] = '0'; 
+                    temp.push(1000 * ny + nx);
+                }
+                else q.push(1000 * ny + nx); 
+            }
+        }
+        q = temp;  
     }
-    cout << cnt;
-    return 0;
+    printf("%d\n", visited[y2][x2]); 
 }
