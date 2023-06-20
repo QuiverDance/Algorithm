f556823397987ce1d16c98621a5cbf2a3e754eaa
여름 <pjw7200@ajou.ac.kr>
2023-06-21 00:28:26 +0900

    second version of #1269

--- week6/1269.cpp
+++ week6/1269.cpp
@@ -1,39 +1,21 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 #include<map>
 
 using namespace std;
 
-int n, m, cnt;
-vector<int> a, b;
-map<int, int> mp1, mp2;
+int n, m, cnt, temp;
+map<int, int> _map;
 
-int main(void){
-    cin >> n >> m;
-    int tmp;
-    for(int i = 0; i < n; i++){ cin >> tmp; a.push_back(tmp); mp1[tmp]++;}
-    for(int i = 0; i < m; i++){ cin >> tmp; b.push_back(tmp); mp2[tmp]++;}
-    sort(a.begin(), a.end()); sort(b.begin(), b.end());
-    
-    for(int i = 0; i < n; i++){
-        int lo = 0, hi = m - 1;
-        int mid;
-        while(lo <= hi){
-            mid = (lo + hi) / 2;
-            if(a[i] == b[mid]){
-                cnt += mp2[b[mid]];
-                if(mp1[a[i]] == 1) cnt++;
-                else{
-                    cnt += mp1[a[i]];
-                    i += mp1[a[i]];
-                }
-                break;
-            }
-            if(a[i] > b[mid]) lo = mid + 1;
-            else hi = mid - 1;
-        }
-    }
-    cout << n + m - cnt;
-    return 0;
+int main(){
+	cin >> n >> m; 
+	for(int i = 0; i < n; i++){
+		cin >> temp; _map[temp]++; 
+	}
+	for(int i = 0; i < m; i++){
+		cin >> temp; _map[temp]++; 
+	}
+	for(auto it : _map){
+		if(it.second == 1)cnt++; 
+	}
+	cout << cnt << "\n"; 
 }
