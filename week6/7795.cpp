0ff607b57462a1b078f5570645caed52b94b148a
여름 <pjw7200@ajou.ac.kr>
2023-06-21 00:29:14 +0900

    second version of #7795

--- week6/7795.cpp
+++ week6/7795.cpp
@@ -1,39 +1,29 @@
 #include<iostream>
 #include<vector>
-#include<cstring>
 #include<algorithm>
 
 using namespace std;
 
-int t, n, m, ret;
-vector<int> a, b;
+typedef long long ll;  
 
-int main(void){
-    cin >> t;
-    int tmp;
+int t, n, m; 
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL); 
+    cin >> t; 
     while(t--){
-        ret = 0; a.clear(); b.clear();
-        cin >> n >> m;
-        for(int i = 0; i < n; i++){ cin >> tmp; a.push_back(tmp);}
-        for(int i = 0; i < m; i++){ cin >> tmp; b.push_back(tmp);}
-        sort(a.begin(), a.end()); sort(b.begin(), b.end());
+        int ret = 0; 
+        cin >> n >> m; 
+        vector<int> a(n), b(m); 
+        for(int i = 0; i < n; i++) cin >> a[i]; 
+        for(int i = 0; i < m; i++) cin >> b[i]; 
+        sort(b.begin(), b.end()); 
+        sort(a.begin(), a.end()); 
         for(int i = 0; i < n; i++){
-            if(a[i] > b[m - 1]){ ret += m; continue;}
-            if(a[i] < b[0]) continue;
-            int lo = 0, hi = m - 1;
-            int mid = (lo + hi) / 2;
-            while(lo <= hi){
-                if(a[i] > b[mid]) lo = mid + 1;
-                else hi = mid - 1;
-                mid = (lo + hi) / 2;
-            }
-            if(a[i] > b[mid])
-                ret += mid + 1;
-            else
-                ret += mid;
-            
+            auto pos = lower_bound(b.begin(), b.end(), a[i]); 
+            ret += (int)(pos - b.begin()); 
         }
-        cout << ret << '\n';
-    }
-    return 0;
+        cout << ret << "\n";  
+    } 
 }
