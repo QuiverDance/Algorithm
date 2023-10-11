38952e4e1d59742291fcd75cda22ba1d5e959cad
여름 <pjw7200@ajou.ac.kr>
2023-10-11 23:58:34 +0900

    temp review 3-P(#14620)

---
+++ review/3-P.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int n, a[104][104], flower[104][104];
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) cin >> a[i][j];
+    }
+    return 0;
+}
