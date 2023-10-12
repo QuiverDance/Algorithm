88bdae83d4782bd8c2ba7cfa35c5383aab1904fd
여름 <pjw7200@ajou.ac.kr>
2023-10-12 23:59:42 +0900

    temp review

--- review/3-P.cpp
+++ review/3-P.cpp
@@ -1,11 +1,37 @@
 #include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
 
-int n, a[104][104], flower[104][104];
+int n, a[104][104], ret = 1e9;
+set<pair<int, int>> flowers;
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
+int solve(vector<int> seeds){
+    for(int i = 0; i < 3; i++){
+        int y = seeds[i] / n;
+        int x = seeds[i] % n;
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx + x + dx[i]; 
+        }
+    }
+}
+
 int main(){
     cin >> n;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++) cin >> a[i][j];
     }
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            for(int k = j + 1; k < n; k++){
+                flowers.clear();
+                ret = min(ret , solve({i, j, k}));
+            }
+        }
+    }
+    cout << ret;
     return 0;
 }
