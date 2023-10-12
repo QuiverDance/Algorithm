ba0bf69c15ebe3768e92923d5140fcc58bf8d99a
여름 <pjw7200@ajou.ac.kr>
2023-10-13 00:05:28 +0900

    review 3-P(#14620)

--- review/3-P.cpp
+++ review/3-P.cpp
@@ -9,14 +9,22 @@ int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 
 int solve(vector<int> seeds){
+    int cost = 0;
     for(int i = 0; i < 3; i++){
         int y = seeds[i] / n;
         int x = seeds[i] % n;
+        flowers.insert({y, x});
+        cost += a[y][x];
         for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
-            int nx + x + dx[i]; 
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+            cost += a[ny][nx];
+            flowers.insert({ny, nx});
         }
     }
+    if(flowers.size() == 15) return cost;
+    return 1e9; 
 }
 
 int main(){
@@ -24,9 +32,9 @@ int main(){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++) cin >> a[i][j];
     }
-    for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
-            for(int k = j + 1; k < n; k++){
+    for(int i = 0; i < n * n; i++){
+        for(int j = i + 1; j < n * n; j++){
+            for(int k = j + 1; k < n * n; k++){
                 flowers.clear();
                 ret = min(ret , solve({i, j, k}));
             }
