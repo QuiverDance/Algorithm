5382d9070293c460ad4b61a5f645d0cfda09cda6
여름 <pjw7200@ajou.ac.kr>
2023-10-17 00:15:06 +0900

    review 4-A(#19942)

--- review/4-A.cpp
+++ review/4-A.cpp
@@ -5,12 +5,24 @@
 using namespace std;
 
 typedef struct Nutrition_{
-    int p, f, s, v, cost;
+    int p = 0, f = 0, s = 0, v = 0, cost = 0;
+    void add(Nutrition_ amount){
+        p += amount.p;
+        f += amount.f;
+        s += amount.s;
+        v += amount.v;
+        cost += amount.cost;
+    }
 } Nutrition;
 
 int n, mp, mf, ms, mv, ret = 1e9;
 Nutrition a[16];
 map<int, vector<vector<int>>> min_mp;
+
+bool validation(Nutrition nutrition){
+    if(nutrition.p < mp || nutrition.f < mf || nutrition.s < ms || nutrition.v < mv) return false;
+    return true;
+}
 
 int main(){
     cin >> n >> mp >> mf >> ms >> mv;
@@ -22,20 +34,24 @@ int main(){
         cin >> a[i].cost;
     }
     for(int i = 0; i < (1 << n); i++){
-        int sum = 0;
+        Nutrition sum;
         vector<int> v;
         for(int j = 0; j < n; j++){
             if(i & (1 << j)){
-                sum += a[j].cost;
+                sum.add(a[j]);
                 v.push_back(j + 1);
             }
         }
-        if(sum <= ret){
-            ret = sum;
-            min_mp[sum].push_back(v);
+        if(sum.cost <= ret && validation(sum)){
+            ret = sum.cost;
+            min_mp[ret].push_back(v);
         }
     }
-    cout << ret;
+    if(ret == 1e9){
+        cout << "-1";
+        return 0;
+    }
+    cout << ret << '\n';
     vector<vector<int>> temp = min_mp[ret];
     sort(temp.begin(), temp.end());
     for(int it : temp[0]){
