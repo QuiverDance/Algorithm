3cc36bcff4911a1aaedfc073c200bee64c4dceeb
여름 <pjw7200@ajou.ac.kr>
2023-11-03 23:32:14 +0900

    review 4-I(#11723)

---
+++ review/4-I.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+using namespace std;
+
+int n, set, target;
+char s[10];
+int main(void){
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%s %d", s, &target);
+        if(s[0] == 'a' && s[1] == 'd')
+            set |= (1 << target);
+        else if(s[0] == 'r')
+            set &= ~(1 << target);
+        else if(s[0] == 'c')
+            set & (1 << target) ? printf("1\n") : printf("0\n");
+        else if(s[0] == 't')
+            set ^= (1 << target);
+        else if(s[0] == 'a')
+            set = (1 << 21) - 1;
+        else
+            set = 0;
+    }
+    return 0;
+}
