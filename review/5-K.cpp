3914932ea0ec798974d34767838bd3b5c75e6e8e
여름 <pjw7200@ajou.ac.kr>
2023-12-06 00:33:00 +0900

    review 5-K(#17144)

--- review/5-K.cpp
+++ review/5-K.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 int r, c, t, ret;
 int a[54][54], temp[54][54];
-vector<int> v1, v2;
+vector<pair<int, int>> v1, v2;
 int dy1[] = {0, -1, 0, 1};
 int dx1[] = {1, 0, -1, 0};
 int dy2[] = {0, 1, 0, -1};
 int dx2[] = {1, 0, -1, 0};
 
 void spread(){
-    memset(temp, 0, sizeof(0));
+    memset(temp, 0, sizeof(temp));
     queue<pair<int, int>> q;
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
@@ -25,12 +25,44 @@ void spread(){
     while(q.size()){
         tie(y, x) = q.front();
         q.pop();
+        int amount = a[y][x] / 5;
         for(int i = 0; i < 4; i++){
             int ny = y + dy1[i];
             int nx = x + dx1[i];
-            if(ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+            if(ny < 0 || nx < 0 || ny >= r || nx >= c || a[ny][nx] == -1) continue;
+            temp[ny][nx] += amount;
+            a[y][x] -= amount;
         }
     }
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            a[i][j] += temp[i][j];
+        }
+    }
+}
+
+vector<pair<int, int>> makeSection(int sy, int sx, int dy[], int dx[]){
+    vector<pair<int, int>> section;
+    int y = sy, x = sx, d = 0;
+    while(true){
+        int ny = y + dy[d];
+        int nx = x + dx[d];
+        if(ny < 0 || nx < 0 || ny >= r || nx >= c){
+            d++;
+            ny = y + dy[d];
+            nx = x + dx[d];
+        }
+        if(ny == sy && nx == sx) break;
+        section.push_back({ny, nx});
+        y = ny; x = nx;
+    }
+    return section;
+}
+
+void work(vector<pair<int, int>> section){
+    for(int i = section.size() - 1; i > 0; i--)
+        a[section[i].first][section[i].second] = a[section[i - 1].first][section[i - 1].second];
+    a[section[0].first][section[0].second] = 0; 
 }
 
 int main(){
@@ -39,14 +71,20 @@ int main(){
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
             cin >> a[i][j];
-            if(!flag) v1 = makeSection(i, j, dy1, dx1);
-            else v2 = makeSection(i, j, dy2, dx2);
+            if(!flag && a[i][j] == -1) { v1 = makeSection(i, j, dy1, dx1); flag = 1; }
+            else if(flag && a[i][j] == -1) v2 = makeSection(i, j, dy2, dx2);
         }
     }
+
     while(t--){
         spread();
         work(v1);
         work(v2);
+    }
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            if(a[i][j] > 0) ret += a[i][j];
+        }
     }
     cout << ret;
     return 0;
