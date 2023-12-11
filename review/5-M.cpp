5d11043b562135a39f4b47551193e4cb06b5a2f9
여름 <pjw7200@ajou.ac.kr>
2023-12-12 00:24:17 +0900

    review 5-M(#12100)

--- review/5-M.cpp
+++ review/5-M.cpp
@@ -1,45 +1,69 @@
 #include<iostream>
+#include<cstring>
+
 using namespace std;
 
-int n, ret, a[24][24];
+int ret, n;
 
-int* move(int board[24][24])[24]{
-    for(int i = 0; i < n; i++){
-        int pos = -1, flag = 0;
-        for(int j = 0; j < n; j++){
-            if(board[i][j] == 0) continue;
-            if(flag == 1 && board[i][j] == board[i][pos]){
-                flag = 0; board[i][pos] *= 2; 
-            }
-            else{
-                flag = 1; board[i][++pos] = board[i][j];
-            }
-        }
-        for(int j = pos + 1; j < n; j++) board[i][j] = 0;
-    }
-    return board;
-}
-
-void solve(int idx, int origin[24][24]){
-    if(idx == 5){
+struct Board{
+    int a[24][24];
+    void _rotate90(){
+        int temp[24][24];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                ret = max(ret, origin[i][j]);
+                temp[i][j] = a[n - j - 1][i];
+            }
+        }
+        memcpy(a, temp, sizeof(a));
+    }
+    void _move(){
+		int temp[24][24];
+		for(int i = 0; i < n; i++){
+			int c = -1, d = 0;
+			for(int j = 0; j < n; j++){
+				if(a[i][j] == 0) continue;
+				if(d && a[i][j] == temp[i][c]) temp[i][c] *= 2, d = 0;
+				else temp[i][++c] = a[i][j], d = 1;
+			}
+			for(c++; c < n; c++) temp[i][c] = 0;
+		}
+		memcpy(a, temp, sizeof(a));
+	}
+    void get_max(){
+        for(int i = 0;i < n; i++){
+            for(int j = 0; j < n; j++){
+                ret = max(ret, a[i][j]);
             }
         }
     }
-    for(int i = 0; i < 4; i++){
-        int temp[24][24] = move(origin);
-        solve(idx + 1, rotate(temp));
+};
+
+void go(Board c, int idx){
+    if(idx == 5){
+        c.get_max();
+        return;
     }
+    for(int i = 0; i < 4; i++){
+        Board d = c;
+        d._move();
+        go(d, idx + 1);
+        c._rotate90();
+    }
+    return;
 }
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> n;
+    Board c;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++) cin >> a[i][j];
+        for(int j = 0; j < n; j++){
+            cin >> c.a[i][j];
+        }
     }
-    solve(0, a);
-    cout << ret;
+    go(c, 0);
+    cout << ret << "\n";
     return 0;
 }
