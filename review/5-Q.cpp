8b62fce53c7be02408583aae66b0a478514ab8ad
여름 <pjw7200@ajou.ac.kr>
2023-12-30 13:50:51 +0900

    review 5-Q(#1911)

--- review/5-Q.cpp
+++ review/5-Q.cpp
@@ -3,7 +3,24 @@
 #include<vector>
 using namespace std;
 
+int n, l, ret;
+vector<pair<int, int>> v;
 int main(){
+    cin >> n >> l;
+    int s, e;
+    for(int i = 0; i < n; i++){
+        cin >> s >> e;
+        v.push_back({s, e});
+    }
+    sort(v.begin(), v.end());
 
+    int cur = 0;
+    for(pair<int, int> it : v){
+        if(cur < it.first) cur = it.first;
+        while(cur < it.second){
+            cur += l; ret++;
+        }
+    }
+    cout << ret;
     return 0;
 }
