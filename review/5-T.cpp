f34eb87a2a0882ebb8c438050292a03c91a2619e
여름 <pjw7200@ajou.ac.kr>
2024-01-12 00:00:01 +0900

    temp

--- review/5-T.cpp
+++ review/5-T.cpp
@@ -2,7 +2,20 @@
 #include<vector>
 using namespace std;
 
-int main(){
+typedef struct _Shark{
+    int r, c, s, d, z;
+}Shark;
 
+int r, c, m, a[104][104], ret;
+vector<Shark> v;
+int main(){
+    cin >> r >> c >> m;
+    int t1, t2, t3, t4, t5;
+    for(int i = 0; i < m; i++){
+        cin >> t1 >> t2 >> t3 >> t4 >> t5;
+        Shark sh;
+        sh.r = t1; sh.c = t2; sh.s = t3; sh.d = t4; sh.z = t5;
+        v.push_back(sh);
+    }
     return 0;
 }
