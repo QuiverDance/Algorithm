3b94833bba861936f7acc98624af75464618288d
여름 <pjw7200@ajou.ac.kr>
2024-01-25 23:58:48 +0900

    #11727 is solved

--- study/11727.cpp
+++ study/11727.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int n, dp[1004];
 int main(){
     cin >> n;
-    dp[1] = 1; dp[2] = 2;
+    dp[1] = 1; dp[2] = 3;
     for(int i = 3; i <= n; i++){
         dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
     }
