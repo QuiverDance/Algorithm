2d3f37fc2f6e6a64b6aa3ad8d9fddf55e35b73cb
여름 <pjw7200@ajou.ac.kr>
2024-02-27 22:43:36 +0900

    #11279 is solved

---
+++ studyS2/11279.cpp
@@ -0,0 +1,25 @@
+/*
+    #11279 최대 힙
+*/
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+int n;
+priority_queue<int, vector<int>, less<int>> pq;
+int main(){
+    scanf("%d", &n);
+    int num;
+    for(int i = 0; i < n; i++){
+        scanf("%d", &num);
+        if(num == 0){
+            if(pq.empty()) cout << 0 << '\n';
+            else cout << pq.top() << '\n', pq.pop();
+        }
+        else{
+            pq.push(num);
+        }
+    }
+    return 0;
+}
