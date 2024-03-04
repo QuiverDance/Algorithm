ea6f23a69a99940f904a4edf0ba1b95df8fda25b
여름 <pjw7200@ajou.ac.kr>
2024-03-04 23:53:54 +0900

    #1182 is solved

---
+++ studyS2/1182.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int n, s, a[24], ret;
+void solve(int idx, int visited, int sum){
+    if(idx == n){
+        if(sum == s && visited != 0) ret++;
+        return;
+    }
+    solve(idx + 1, visited | (1 << idx), sum + a[idx]);
+    solve(idx + 1, visited, sum);
+}
+
+int main(){
+    cin >> n >> s;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    solve(0, 0, 0);
+    cout << ret;
+    return 0;
+}
