bc7aa140f3316e7dc9b6d965e3480862e0153db8
여름 <pjw7200@ajou.ac.kr>
2024-03-18 23:59:14 +0900

    #2156 is solved:

---
+++ studyS1/2156.cpp
@@ -0,0 +1,20 @@
+/*
+    #2156 포도주 시식
+*/
+#include<iostream>
+using namespace std;
+
+int n, a[10004], dp[10004], ret;
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+    dp[1] = a[1]; 
+    dp[2] = a[1] + a[2];
+    for(int i = 3; i <= n; i++){
+		dp[i] = max(dp[i - 1], max(dp[i - 3] + a[i - 1] + a[i], dp[i - 2] + a[i]));
+    }
+    cout << dp[n];
+    return 0;
+}
