aece8a70f2e75dcb4a296b2ff12a7d30e5731afb
여름 <pjw7200@ajou.ac.kr>
2024-03-30 16:18:30 +0900

    #1946 is solved

--- studyS1/1946.cpp
+++ studyS1/1946.cpp
@@ -6,7 +6,7 @@
 #include<algorithm>
 using namespace std;
 
-int t, n, ret, flag, mn;
+int t, n, ret, mn;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -24,7 +24,6 @@ int main() {
 
         ret = 0, mn = 1e9;
         for(int i = 0; i < n; i++){
-            flag = 1;
             if(v[i].second > mn) continue;
             mn = min(mn, v[i].second);
             ret++;
