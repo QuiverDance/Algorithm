19798a202f5e8b38762805511ccf1cc137dd0d5f
여름 <pjw7200@ajou.ac.kr>
2024-04-29 23:50:58 +0900

    #1107 is solved

---
+++ studyG5/1107.cpp
@@ -0,0 +1,33 @@
+/*
+    #1107 리모컨
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int n, m, l, valid[14], ret;
+void solve(string s){
+    if(s.length() > 6){ return;}
+    if(s != "")
+        ret = min(ret, (int)s.length() + abs(stoi(s) - n));
+    for(int i = 0; i < 10; i++){
+        if(valid[i]) continue;
+        s.push_back(i + '0');
+        solve(s);
+        s.pop_back();
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    if(m > 0){
+        int num;
+        for(int i = 0; i < m; i++){
+            cin >> num; valid[num] = 1;
+        }
+    }
+    ret = abs(n - 100); l = to_string(n).length();
+    solve("");
+    cout << ret;
+    return 0;
+}
