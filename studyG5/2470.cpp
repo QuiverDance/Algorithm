8a717643da6425286755bb19320e5b8c42a98532
여름 <pjw7200@ajou.ac.kr>
2024-05-02 23:16:14 +0900

    #2470 is solved

---
+++ studyG5/2470.cpp
@@ -0,0 +1,34 @@
+/*
+    #2470 두 용액
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, ret1, ret2, mn = 2e9;
+vector<int> v;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    
+    int num;
+    for(int i = 0; i < n; i++){
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    int lo = 0, hi = n - 1;
+    while(lo < hi){
+        // cout << v[lo] << " " << v[hi] << '\n'; 
+        if(mn > abs(v[lo] + v[hi])){
+            mn = abs(v[lo] + v[hi]);
+            ret1 = v[lo]; ret2 = v[hi];
+        }
+        if(v[lo] + v[hi] < 0) lo++;
+        else hi--;
+    }
+    cout << ret1 << " " << ret2;
+    return 0;
+}
